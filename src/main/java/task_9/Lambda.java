e3f28eada7b64294cda1fe1eb4a2e5d780635db7
sark@bk.ru <Gir1i9n8x3>
2020-05-31 13:11:43 +0300

    test completed

--- src/main/java/task_9/Lambda.java
+++ src/main/java/task_9/Lambda.java
@@ -1,5 +1,22 @@
 package task_9;
 
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Lambda {
+
+    public static void main(String[] args) {
+        List<String> str1 = new ArrayList<String>();
+        str1.add("asdfsadf");
+        str1.add("asda");
+        str1.add("asdfasdgadfgdafgasd");
+        str1.add("a");
+
+        System.out.println(str1.size());
+
+
+    }
 
 }
